cb5edf543976d5c2dfbe8e72317557d8bb3730e7
Spencer Smith <spencersmith24@outlook.com>
2025-01-25 16:18:31 -0500

    change hitboxes and move files

--- Project/group-prototype/GameObjects/Enemy/enemy.gd
+++ Project/group-prototype/GameObjects/Enemy/enemy.gd
@@ -1,6 +1,32 @@
 extends CharacterBody2D
+@export var speed= 120
+@export var direction = Vector2.UP
 
-@export var SPEED: float = 2.0
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
 
-func _process(_delta: float) -> void:
-	get_parent().progress += SPEED
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	velocity = direction *speed
+	move_and_slide()
+	
+func moveUp():
+	direction = Vector2.UP
+func moveDown():
+	direction = Vector2.DOWN
+func moveRight():
+	direction = Vector2.RIGHT
+func moveLeft():
+	
+	direction = Vector2.LEFT
+	
+func getDirection():
+	return direction
+
+
+func _on_hitbox_body_entered(body: Node2D) -> void:
+	if body.name.match("Bullet"):
+		body.queue_free()
+		self.queue_free()
