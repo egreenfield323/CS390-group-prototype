6c2ad14d6fc2f448970a36596b968a24e450ef74
Spencer Smith <spencersmith24@outlook.com>
2025-01-25 15:57:07 -0500

    create clone shooting mechanic

--- Project/group-prototype/GameObjects/Clone/clone.gd
+++ Project/group-prototype/GameObjects/Clone/clone.gd
@@ -1,11 +1,17 @@
 extends RigidBody2D
 
+@export var BULLET_RESOURCE: Resource
+@onready var BULLET_NODE = $Bullets
+@onready var SHOOT_TIMER = $ShootTimer
+
 var enemies_in_view = []
 var looking = false
+
 
 func _process(delta: float) -> void:
 	if check_looking():
 		look_at(enemies_in_view[0].global_position)
+		shoot_bullet()
 
 func _on_clone_radius_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Enemies"):
@@ -20,3 +26,11 @@ func check_looking():
 		return false
 	
 	return true
+
+func shoot_bullet():
+	if SHOOT_TIMER.time_left <= 0:
+		var bullet = BULLET_RESOURCE.instantiate()
+		bullet.position = get_parent().global_position
+		bullet.direction = (self.global_position - enemies_in_view[0].global_position).normalized()
+		BULLET_NODE.add_child(bullet)
+		SHOOT_TIMER.start()
