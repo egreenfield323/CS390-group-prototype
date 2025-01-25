3ac8dbbb61b7e48937885ca3d1052f5998e6016c
Spencer Smith <spencersmith24@outlook.com>
2025-01-25 18:44:43 -0500

    add rounds in level 1

--- Project/group-prototype/Scenes/Levels/level_1.gd
+++ Project/group-prototype/Scenes/Levels/level_1.gd
@@ -2,12 +2,67 @@ extends Node2D
 
 @onready var ENEMY_SPAWN_POINT = $EnemySpawnPoint.position
 @onready var ENEMY_NODE = $Enemies
+@onready var SPAWN_TIMER = $EnemySpawnPoint/SpawnTimer
 @export var ENEMY_RESOURCE: Resource
 
+var enemy_array = []
+var round_complete : bool = false
+var current_round : int = 0
+var next_round : int = 1
 
+# start level
+func _on_play_button_pressed() -> void:
+	$Control/PlayButton.disabled = true
+	start_level()
 
 func _on_spawn_timer_timeout() -> void:
+	spawn_enemy()
+
+func create_enemy(level: int):
 	var enemy = ENEMY_RESOURCE.instantiate()
 	enemy.global_position = ENEMY_SPAWN_POINT
 	enemy.direction = Vector2(1,0)
-	ENEMY_NODE.add_child(enemy)
+	enemy.level = level
+	return enemy
+
+func spawn_enemy():
+	if enemy_array.is_empty():
+		SPAWN_TIMER.stop()
+		return
+	
+	ENEMY_NODE.add_child(enemy_array[0])
+	enemy_array.remove_at(0)
+
+func start_level():
+	round_three()
+
+# LEVEL ROUNDS
+
+# ROUND 1: 20 REDS
+func round_one():
+	current_round = 1
+	next_round = 2
+	for i in 20:
+		enemy_array.append(create_enemy(1))
+	
+	SPAWN_TIMER.start()
+
+# ROUND 2: 10 REDS, 10 BLUES
+func round_two():
+	current_round = 2
+	for i in 10:
+		enemy_array.append(create_enemy(1))
+		enemy_array.append(create_enemy(2))
+	
+	SPAWN_TIMER.start()
+
+# ROUND 3: 15 REDS, 15 BLUES, 5 GREENS
+func round_three():
+	current_round = 3
+	for i in 5:
+		enemy_array.append(create_enemy(3))
+		for j in 3:
+			enemy_array.append(create_enemy(1))
+			enemy_array.append(create_enemy(2))
+	
+	SPAWN_TIMER.start()
