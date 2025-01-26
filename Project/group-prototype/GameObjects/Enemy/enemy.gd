cc7feaa4cf3fa57403db38d706a5161002c69427
Spencer Smith <spencersmith24@outlook.com>
2025-01-26 10:32:16 -0500

    add two new enemy levels

--- Project/group-prototype/GameObjects/Enemy/enemy.gd
+++ Project/group-prototype/GameObjects/Enemy/enemy.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var speed = 120
 @export var health : int = 1
 @export var level : int = 1
+@export var worth : int = 5
 @export var direction = Vector2.UP
 
 # Called when the node enters the scene tree for the first time.
@@ -12,14 +13,28 @@ func _ready() -> void:
 			$ColorRect.color = Color(0.99, 0.19, 0.17, 1.0)
 			health = 1
 			speed = 120
+			worth = 5
 		2:
 			$ColorRect.color = Color(0.0, 1.0, 1.0, 1.0)
 			health = 2
 			speed = 110
+			worth = 10
 		3:
 			$ColorRect.color = Color(0.0, 0.76, 0.21, 1.0)
 			health = 3
 			speed = 125
+			worth = 15
+		4:
+			$ColorRect.color = Color(0.84, 0.34, 0.83, 1.0)
+			health = 5
+			speed = 110
+			worth = 30
+		5:
+			$ColorRect.color = Color(0.89, 0.49, 0.21, 1.0)
+			health = 10
+			speed = 90
+			worth = 50
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
